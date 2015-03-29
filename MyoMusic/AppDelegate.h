87c7ac2024ba8910d74569c0b2eceffb79c9fc0b
Katelyn Findlay <kcfin@umich.edu>
2015-03-29 19:52:58 -0400

    Initial Commit

---
+++ MyoMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyoMusic
+//
+//  Created by Kate Findlay on 3/29/15.
+//  Copyright (c) 2015 EECS493. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
