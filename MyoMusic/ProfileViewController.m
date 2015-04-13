4725028a9f47b90b84f599a713b992a732c5bc32
Katelyn Findlay <kcfin@umich.edu>
2015-04-13 12:12:35 -0400

    Working on profile page

--- MyoMusic/ProfileViewController.m
+++ MyoMusic/ProfileViewController.m
@@ -13,6 +13,7 @@
 
 @interface ProfileViewController () <UITableViewDataSource, UITableViewDelegate>
 
+@property (nonatomic) UIScrollView *scrollView;
 @property (nonatomic) UITableView *tableView;
 @property (nonatomic) UIView *userView;
 @property (nonatomic) UIImageView *profileImageView;
@@ -35,20 +36,37 @@
     
     self.userView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height-self.tableView.frame.size.height)];
     [self.view addSubview:self.userView];
-    self.userView.backgroundColor = [UIColor greenColor];
+    self.userView.backgroundColor = [UIColor darkGrayColor];
     
     self.user = [SpotifyUser user];
+    [self loadLabel];
     [self loadProfilePicture];
 }
 
 -(void)reload {
     self.profileImageView.image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:self.user.sptUser.largestImage.imageURL]];
-    
+    self.nameLabel.text = self.user.sptUser.displayName;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (void)loadScrollView {
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    self.scrollView.alwaysBounceVertical = YES;
+    self.scrollView.delaysContentTouches = NO;
+    self.scrollView.backgroundColor = [UIColor darkGrayColor];
+//    [self.view addSubview:self.scrollView];
+}
+
+-(void)loadLabel {
+    self.nameLabel = [UILabel new];
+    self.nameLabel.font = [UIFont fontWithName:@"Avenir-Heavy" size:[UIFont systemFontSize] * 2];
+    self.nameLabel.textColor = [UIColor whiteColor];
+    self.nameLabel.frame = CGRectMake(self.view.center.x, 100, self.nameLabel.frame.size.width, self.nameLabel.frame.size.height);
+    [self.userView addSubview:self.nameLabel];
 }
 
 -(void)loadProfilePicture {
@@ -58,7 +76,6 @@
     self.profileImageView.layer.masksToBounds = YES;
     self.profileImageView.frame = CGRectMake(0, 0, self.userView.frame.size.width/3, self.userView.frame.size.width/3);
     self.profileImageView.center = self.userView.center;
-    self.profileImageView.backgroundColor = [UIColor blueColor];
     [self.userView addSubview:self.profileImageView];
     
 }
