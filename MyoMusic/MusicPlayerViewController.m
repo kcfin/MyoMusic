254906e9872412d4a864fd9e8064cb93bbdc7689
Alex <aathans@umich.edu>
2015-04-15 02:49:15 -0400

    playback, skip, prev, volume, fixed orient.

--- MyoMusic/MusicPlayerViewController.m
+++ MyoMusic/MusicPlayerViewController.m
@@ -16,15 +16,22 @@
 @property (nonatomic)NSMutableArray *trackURIs;
 @property (nonatomic)SPTTrack *currentTrack;
 @property (nonatomic)SPTArtist *currentArtist;
+@property (nonatomic)NSInteger currentIndex;
 
 @property (nonatomic)UILabel *trackLabel;
 @property (nonatomic)UILabel *artistLabel;
 @property (nonatomic)UIImageView *coverArt;
+@property (nonatomic)UIImage *playImage;
+@property (nonatomic)UIImage *pauseImage;
 
 @property (nonatomic)UIButton *playButton;
 @property (nonatomic)UIButton *pauseButton;
 @property (nonatomic)UIButton *nextButton;
 @property (nonatomic)UIButton *prevButton;
+
+@property (nonatomic)UISlider *trackSlider;
+@property (nonatomic)UISlider *volumeSlider;
+@property (nonatomic)NSTimer *playbackTimer;
 
 -(void)updateInfo;
 
@@ -37,32 +44,60 @@
     //UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
     self.title = @"Now Playing";
     self.trackURIs = [NSMutableArray new];
+    self.currentIndex = 0;
+
+    self.volumeSlider = [UISlider new];
+    [self.volumeSlider addTarget:self action:@selector(volumeChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.volumeSlider setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.volumeSlider setContinuous:YES];
+    [self.volumeSlider setMinimumValue:0.0f];
+    [self.volumeSlider setValue:0.5f];
+    [self.volumeSlider setMaximumValue:1.0f];
     
+    UIImage *trackImage = [UIImage imageNamed:@"mySlider.png"];
+    self.trackSlider = [UISlider new];
+    [self.trackSlider setThumbImage:trackImage forState:UIControlStateNormal];
+    [self.trackSlider addTarget:self action:@selector(sliderActive:) forControlEvents:UIControlEventTouchUpInside];
+    [self.trackSlider setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.trackSlider setMinimumValue:0];
+    [self.trackSlider setContinuous:YES];
+    
+    self.playImage = [UIImage imageNamed:@"playButton.png"];
+    self.pauseImage = [UIImage imageNamed:@"pauseButton.png"];
     self.playButton = [UIButton new];
+    [self.playButton setImage:self.pauseImage forState:UIControlStateNormal];
     [self.playButton addTarget:self action:@selector(togglePlaying:) forControlEvents:UIControlEventTouchUpInside];
     [self.playButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    UIImage *nextImage = [UIImage imageNamed:@"nextButton.png"];
     self.nextButton = [UIButton new];
-    [self.nextButton addTarget:self action:@selector(skipNext:) forControlEvents:UIControlEventTouchUpInside];
+    [self.nextButton setImage:nextImage forState:UIControlStateNormal];
+    [self.nextButton addTarget:self action:@selector(nextButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [self.nextButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    UIImage *prevImage = [UIImage imageNamed:@"prevButton.png"];
     self.prevButton = [UIButton new];
-    [self.prevButton addTarget:self action:@selector(skipPrevious:) forControlEvents:UIControlEventTouchUpInside];
+    [self.prevButton setImage:prevImage forState:UIControlStateNormal];
+    [self.prevButton addTarget:self action:@selector(prevButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [self.prevButton setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     self.trackLabel = [UILabel new];
     //[self.trackLabel setFrame:CGRectMake(50, 300, 250, 50)];
-    [self.trackLabel setTextColor:[UIColor redColor]];
+    [self.trackLabel setTextColor:[UIColor whiteColor]];
+    [self.trackLabel setClipsToBounds:YES];
     [self.trackLabel setLineBreakMode:NSLineBreakByTruncatingTail];
-    self.trackLabel.backgroundColor = [UIColor whiteColor];
-    self.trackLabel.numberOfLines = 2;
+    self.trackLabel.backgroundColor = [UIColor blackColor];
+    self.trackLabel.numberOfLines = 1;
     [self.trackLabel setTextAlignment:NSTextAlignmentCenter];
     [self.trackLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     self.artistLabel = [UILabel new];
     //[self.artistLabel setFrame:CGRectMake(100, 350, 200, 50)];
-    self.artistLabel.backgroundColor = [UIColor whiteColor];
-    [self.artistLabel setTextColor:[UIColor redColor]];
+    [self.artistLabel setLineBreakMode:NSLineBreakByTruncatingTail];
+    [self.artistLabel setClipsToBounds:YES];
+    self.artistLabel.backgroundColor = [UIColor blackColor];
+    [self.artistLabel setTextColor:[UIColor whiteColor]];
+    [self.artistLabel setTextAlignment:NSTextAlignmentCenter];
     [self.artistLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     self.coverArt = [UIImageView new];
@@ -71,6 +106,8 @@
     
     [self.view setCenter:CGPointMake([UIScreen mainScreen].bounds.size.width/2, [UIScreen mainScreen].bounds.size.height/2)];
     
+    [self.view addSubview:self.volumeSlider];
+    [self.view addSubview:self.trackSlider];
     [self.view addSubview:self.trackLabel];
     [self.view addSubview:self.artistLabel];
     [self.view addSubview:self.coverArt];
@@ -81,8 +118,8 @@
 
     [self createConstraints];
     
-    [self.playButton setBackgroundColor:[UIColor whiteColor]];
-    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.view.backgroundColor = [UIColor blackColor];
+
     // Do any additional setup after loading the view.
 }
 
@@ -93,18 +130,36 @@
     UIView *playView = self.playButton;
     UIView *nextView = self.nextButton;
     UIView *prevView = self.prevButton;
+    UIView *playbackView = self.trackSlider;
+    UIView *volumeView = self.volumeSlider;
     
-    NSDictionary *views = NSDictionaryOfVariableBindings(trackView, artistView, coverView, playView, nextView, prevView, self.view);
+    NSDictionary *views = NSDictionaryOfVariableBindings(trackView, artistView, coverView, playView, nextView, prevView, playbackView, volumeView, self.view);
     
     NSArray *constraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:[coverView(<=200)]" options:NSLayoutFormatAlignAllCenterX metrics:nil views:views];
     
     constraints = [constraints arrayByAddingObject:[NSLayoutConstraint constraintWithItem:self.coverArt attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0]];
     
-    constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-84-[coverView]-[trackView(50)]-10-[artistView(50)]-[playView(50)]" options:NSLayoutFormatAlignAllCenterX metrics:nil views:views]];
+    constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-104-[coverView]-20-[trackView(20)]-10-[artistView(20)]" options:NSLayoutFormatAlignAllCenterX metrics:nil views:views]];
+    
+    constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[playbackView(20)]-20-[playView(50)]-15-[volumeView(20)]-40-|" options:NSLayoutFormatAlignAllCenterX metrics:nil views:views]];
+    
+    constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[trackView]-50-|" options:0 metrics:nil views:views]];
+    
+    constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[artistView]-50-|" options:0 metrics:nil views:views]];
+    
+    constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[playbackView]-50-|" options:0 metrics:nil views:views]];
+    
+    constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[volumeView]-50-|" options:0 metrics:nil views:views]];
     
     constraints = [constraints arrayByAddingObject:[NSLayoutConstraint constraintWithItem:self.playButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.playButton attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0]];
     
     constraints = [constraints arrayByAddingObject:[NSLayoutConstraint constraintWithItem:self.coverArt attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.coverArt attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0]];
+    
+    constraints = [constraints arrayByAddingObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[prevView(50)]-40-[playView]-40-[nextView(50)]" options:NSLayoutFormatAlignAllCenterY metrics:nil views:views]];
+    
+    constraints = [constraints arrayByAddingObject:[NSLayoutConstraint constraintWithItem:self.nextButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.playButton attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0]];
+    
+    constraints = [constraints arrayByAddingObject:[NSLayoutConstraint constraintWithItem:self.prevButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.playButton attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0]];
     
     [self.view addConstraints:constraints];
 
@@ -121,9 +176,11 @@
             if([object isKindOfClass:[SPTPlaylistSnapshot class]]){
                 self.currentPlaylist = (SPTPlaylistSnapshot *)object;
                 NSLog(@"PLAYLIST SIZE: %lu", (unsigned long)self.currentPlaylist.trackCount);
+                unsigned int i = 0;
                 if(self.currentPlaylist.trackCount > 0){
                     for(SPTTrack *track in self.currentPlaylist.tracksForPlayback){
-                        NSLog(@"GOT SONG: %@", track.name);
+                        NSLog(@"GOT SONG:%u %@ ", i, track.name);
+                        i++;
                         [self.trackURIs addObject:track.uri];
                     }
                     [self handleNewSession];
@@ -143,7 +200,11 @@
     if (self.audioPlayer == nil) {
         self.audioPlayer = [[SPTAudioStreamingController alloc] initWithClientId:auth.clientID];
         self.audioPlayer.playbackDelegate = self;
-        self.audioPlayer.diskCache = [[SPTDiskCache alloc] initWithCapacity:1024 * 1024 * 64];
+        SPTVolume volume = 0.5;
+        [self.audioPlayer setVolume:volume callback:^(NSError *error) {
+            
+        }];
+        //self.audioPlayer.diskCache = [[SPTDiskCache alloc] initWithCapacity:1024 * 1024 * 64];
     }
     
     [self.audioPlayer loginWithSession:auth.session callback:^(NSError *error) {
@@ -154,15 +215,20 @@
         }
         
         //[self updateUI];
-        [self.audioPlayer playURIs:self.trackURIs fromIndex:0 callback:^(NSError *error) {
+        [self.audioPlayer playURIs:self.trackURIs fromIndex:self.currentIndex callback:^(NSError *error) {
             if(error != nil){
                 NSLog(@"ERROR");
                 return;
             }
-            self.currentTrack = [self.currentPlaylist.tracksForPlayback objectAtIndex:0];
+            self.currentTrack = [self.currentPlaylist.tracksForPlayback objectAtIndex:self.currentIndex];
             self.trackLabel.text = self.currentTrack.name;
-            SPTPartialArtist *artist = (SPTPartialArtist *)[self.currentTrack.artists objectAtIndex:0];
+            SPTPartialArtist *artist = (SPTPartialArtist *)[self.currentTrack.artists objectAtIndex:self.currentIndex];
             self.artistLabel.text = artist.name;
+            
+            [self.trackSlider setMaximumValue:self.audioPlayer.currentTrackDuration];
+            [self.trackSlider setValue:self.audioPlayer.currentPlaybackPosition animated:YES];
+            self.playbackTimer = [NSTimer scheduledTimerWithTimeInterval:0.1f target:self selector:@selector(updateSlider:) userInfo:nil repeats:YES];
+            NSLog(@"TRACK DURATION: %f", self.audioPlayer.currentTrackDuration);
             
             NSURL *coverArtURL = self.currentTrack.album.largestCover.imageURL;
             if(coverArtURL){
@@ -196,28 +262,87 @@
     }];
 }
 
+-(void)volumeChanged:(id)sender{
+    SPTVolume volume = self.volumeSlider.value;
+    [self.audioPlayer setVolume:volume callback:^(NSError *error) {
+        
+    }];
+}
+
+-(void)sliderActive:(id)sender{
+    [self.audioPlayer seekToOffset:self.trackSlider.value callback:^(NSError *error) {
+        NSLog(@"Changed to offset");
+    }];
+}
+
+-(void)updateSlider:(id)sender{
+    if(!self.trackSlider.isTracking){
+        [self.trackSlider setValue:self.audioPlayer.currentPlaybackPosition animated:YES];
+        [self.trackSlider setMaximumValue:self.audioPlayer.currentTrackDuration];
+        NSLog(@"updating slider value w/ value: %f and max: %f", self.audioPlayer.currentPlaybackPosition, self.audioPlayer.currentTrackDuration);
+    }
+}
+
 -(void)togglePlaying:(id)sender{
+    NSLog(@"Current playback: %f", self.audioPlayer.currentPlaybackPosition);
     if(self.audioPlayer.isPlaying){
         [self.audioPlayer setIsPlaying:NO callback:^(NSError *error) {
-        
         }];
+        [_playButton setImage:self.playImage forState:UIControlStateNormal];
+
     }else{
         [self.audioPlayer setIsPlaying:YES callback:^(NSError *error) {
-            
         }];
+        [_playButton setImage:self.pauseImage forState:UIControlStateNormal];
     }
 }
 
 -(void)nextButtonPressed:(id)sender{
-    [self.audioPlayer skipNext:^(NSError *error) {
-        NSLog(@"Skipped to next song");
+    
+    self.currentIndex++;
+    self.currentIndex = self.currentIndex % self.trackURIs.count;
+    
+    SPTPlayOptions *playOptions = [SPTPlayOptions new];
+    playOptions.startTime = 0;
+    playOptions.trackIndex = self.currentIndex;
+    [self.audioPlayer playURIs:self.trackURIs withOptions:playOptions callback:^(NSError *error) {
+        if(error != nil){
+            NSLog(@"ERROR: %@", error);
+            abort();
+        }
+        [self.playButton setImage:self.pauseImage forState:UIControlStateNormal];
+        self.currentTrack = [self.currentPlaylist.tracksForPlayback objectAtIndex:self.currentIndex];
+        self.trackLabel.text = self.currentTrack.name;
+        SPTPartialArtist *artist = (SPTPartialArtist *)[self.currentTrack.artists objectAtIndex:0];
+        self.artistLabel.text = artist.name;
     }];
+    
+    NSLog(@"Skipped to next song");
 }
 
 -(void)prevButtonPressed:(id)sender{
-    [self.audioPlayer skipPrevious:^(NSError *error) {
-        NSLog(@"Skipped to previous song");
+    if(self.currentIndex == 0){
+        self.currentIndex = self.trackURIs.count-1;
+    }else{
+        self.currentIndex--;
+    }
+    
+    SPTPlayOptions *playOptions = [SPTPlayOptions new];
+    playOptions.startTime = 0;
+    playOptions.trackIndex = self.currentIndex;
+    
+    [self.audioPlayer playURIs:self.trackURIs withOptions:playOptions callback:^(NSError *error) {
+        if(error != nil){
+            NSLog(@"ERROR: %@", error);
+            abort();
+        }
+        [self.playButton setImage:self.pauseImage forState:UIControlStateNormal];
+        self.currentTrack = [self.currentPlaylist.tracksForPlayback objectAtIndex:self.currentIndex];
+        self.trackLabel.text = self.currentTrack.name;
+        SPTPartialArtist *artist = (SPTPartialArtist *)[self.currentTrack.artists objectAtIndex:0];
+        self.artistLabel.text = artist.name;
     }];
+    
 }
 
 #pragma mark - Track Player Delegates
@@ -236,7 +361,12 @@
 }
 
 - (void) audioStreaming:(SPTAudioStreamingController *)audioStreaming didChangeToTrack:(NSDictionary *)trackMetadata {
+    //SPTAuth *auth = [SPTAuth defaultInstance];
+    
     NSLog(@"track changed = %@", [trackMetadata valueForKey:SPTAudioStreamingMetadataTrackURI]);
+//    [SPTRequest requestItemAtURI:[trackMetadata valueForKey:SPTAudioStreamingMetadataTrackURI] withSession:auth.session callback:^(NSError *error, id object) {
+//        SPTTrack
+//    }];
     [self updateInfo];
 }
 
